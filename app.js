2d210fe9a1e555ca1a2b16829733642cb6ffe286
ianbuen <ianbuen@gmail.com>
2020-11-10 19:54:50 +0800

    implement login w/ PassportJS

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 
-// use bcrypt encryption
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+// sessions
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+// connect-flash
+const flash = require('connect-flash');
 
 // make instance of express app
 const app = express();
@@ -14,19 +18,46 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(flash());
+
+// initialize session
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+// initialize passport to manage sessions
+app.use(passport.initialize());
+app.use(passport.session());
 
 // set mongoDB connection via mongoose
-const options = { useUnifiedTopology: true, useNewUrlParser: true };
+const options = { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true };
 mongoose.connect("mongodb://localhost:27017/userDB", options);
 
 // define USER schema
 const userSchema = new mongoose.Schema({
-  email: String,
+  username: String,
   password: String
 });
 
+// add passport plugin to user schema
+userSchema.plugin(passportLocalMongoose);
+
 // create model from schema
 const User = new mongoose.model("User", userSchema);
+
+// let passport handle User auth
+ passport.use(User.createStrategy());
+ passport.serializeUser(User.serializeUser());
+ passport.deserializeUser(User.deserializeUser());
+
+ // passport authenticate options
+ const authOptions = {
+     failureFlash: "Invalid username or password.",
+     failureRedirect: "/login"
+ };
+
 
 // root route
 app.get("/", (req, res) => {
@@ -37,31 +68,18 @@ app.get("/", (req, res) => {
 // login route
 app.route("/login")
    .get((req, res) => {
-        res.render("login");
+        res.render("login", {message: req.flash('error') });
    })
-   .post((req, res) => {
 
-        const email = req.body.username;
-        const pass = req.body.password;
-
-        User.findOne({ email: email }, (err, foundUser) => {
-            if (err)
-                console.log(err);
-            else if (foundUser) {
-                bcrypt.compare(pass, foundUser.password, function(err, match) {
-                    if (match)
-                        res.redirect("/secrets");
-                    else // incorrect password
-                        console.log("Incorrect Username / Password.");
-                });
-            } else // user does not exist
-                console.log("Incorrect Username / Password.");
-        });
-   });
+   // POST LOGIN - 2 functions as parameters (auth and callback)
+   .post(passport.authenticate("local", authOptions), (req, res) => {
+        res.redirect("/secrets");
+   }); // END of POST LOGIN
 
 
 app.route("/logout")
    .get((req, res) => {
+        req.logout();
         res.redirect("/");
    });
 
@@ -69,29 +87,31 @@ app.route("/logout")
 // register route
 app.route("/register")
    .get((req, res) => {
-        res.render("register");
+        res.render("register", { message: "" });
    })
-   .post((req, res) => {
-        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-          const user = new User({
-            email: req.body.username,
-            password: hash
-          });
 
-          user.save((err) => {
-            if (!err)
+   // REGISTER POST METHOD
+   .post((req, res) => {
+     const user = new User({ username: req.body.username});
+
+      User.register(user, req.body.password, (err, acc) => {
+        if (err)
+            res.render("register", { message: err.message });
+        else
+            passport.authenticate("local")(req, res, () => {
                 res.redirect("/secrets");
-            else
-                console.log(err);
-          });
-        });
-   });
+            });
+      });
+   }); // REG POST METHOD END
 
 
 // secrets page route
 app.route("/secrets")
    .get((req, res) => {
-        res.render("secrets");
+        if (req.isAuthenticated())
+            res.render("secrets");
+        else
+            res.redirect("/login");
    });
 
 
@@ -99,3 +119,8 @@ app.route("/secrets")
 app.listen(80, () => {
   console.log("Server started on Port 80.");
 });
+
+
+function echo(object) {
+  console.log(object);
+};
